387313280b304c418c0bb5e6784333f75112c7d7
AbuBakkorSiddique <153945310+AbuBakkorSiddique@users.noreply.github.com>
2024-06-19 18:27:35 +0600

    Update index.js   url

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const PORT = 3000;
+const PORT = "https://crudtwo-kbrr73yzx-abu-bakkor-siddique-s-projects.vercel.app/";
 
 app.listen(PORT, () => {
   console.log(`The server is running on http://localhost:${PORT}`);
